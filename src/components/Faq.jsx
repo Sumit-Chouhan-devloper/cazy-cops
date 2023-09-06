70130079d7e87f65037e9cac763681cfb22f4117
Your Name <sumitkhan.dhana@gmail.com>
2023-09-06 13:52:33 +0530

    accordion item

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,10 +4,7 @@ import Footer from "./Footer";
 const Faqs = () => {
     return (
         <>
-            <section
-                className="position-relative pt-5 faq_bg" id="faq"
-            >
-
+            <section className="position-relative pt-5 faq_bg" id="faq">
                 <Container className="mt-5 position-relative z-3">
                     <Row className="justify-content-center pb-sm-5 mb-sm-5">
                         <Col lg={10}>
@@ -17,6 +14,7 @@ const Faqs = () => {
                             <div className="faqs pt-lg-5 pt-4 mt-lg-2">
                                 <Accordion defaultActiveKey="0">
                                     <Accordion.Item
+                                     eventKey="0"
                                         data-aos="zoom-in"
                                         data-aos-delay="100"
                                         data-aos-anchor-placement="center-bottom"
@@ -34,15 +32,16 @@ const Faqs = () => {
                                         </Accordion.Body>
                                     </Accordion.Item>
                                     <Accordion.Item
-                                                 data-aos="zoom-in"
-                                                 data-aos-delay="200"
-                                                 data-aos-anchor-placement="center-bottom"
+                                     eventKey="1"
+                                        data-aos="zoom-in"
+                                        data-aos-delay="200"
+                                        data-aos-anchor-placement="center-bottom"
                                     >
                                         <Accordion.Header>
                                             Q2. How does Cozy Capys give back to wildlife conservation?
                                         </Accordion.Header>
                                         <Accordion.Body>
-                                            <p className=" ff_Balsamiq_sans fs_sm fw-bold txt_black mb-1">
+                                            <p className="ff_Balsamiq_sans fs_sm fw-bold txt_black mb-1">
                                                 SCal Mobile is a U.S. tech solutions company
                                                 specializing in the recommerce of electronics. We
                                                 offer a wide range of iPhones and services, all at
@@ -51,9 +50,10 @@ const Faqs = () => {
                                         </Accordion.Body>
                                     </Accordion.Item>
                                     <Accordion.Item
-                                                    data-aos="zoom-in"
-                                                    data-aos-delay="300"
-                                                    data-aos-anchor-placement="center-bottom"
+                                     eventKey="2"
+                                        data-aos="zoom-in"
+                                        data-aos-delay="300"
+                                        data-aos-anchor-placement="center-bottom"
                                     >
                                         <Accordion.Header>
                                             Q3. What kind of community events does Cozy Capys host?
@@ -68,9 +68,10 @@ const Faqs = () => {
                                         </Accordion.Body>
                                     </Accordion.Item>
                                     <Accordion.Item
-                                                    data-aos="zoom-in"
-                                                    data-aos-delay="400"
-                                                    data-aos-anchor-placement="center-bottom"
+                                     eventKey="3"
+                                        data-aos="zoom-in"
+                                        data-aos-delay="400"
+                                        data-aos-anchor-placement="center-bottom"
                                     >
                                         <Accordion.Header>
                                             Q4. How can I stay updated on Cozy Capys news and announcements?
@@ -85,9 +86,10 @@ const Faqs = () => {
                                         </Accordion.Body>
                                     </Accordion.Item>
                                     <Accordion.Item
-                                                    data-aos="zoom-in"
-                                                    data-aos-delay="500"
-                                                    data-aos-anchor-placement="center-bottom"
+                                     eventKey="4"
+                                        data-aos="zoom-in"
+                                        data-aos-delay="500"
+                                        data-aos-anchor-placement="center-bottom"
                                     >
                                         <Accordion.Header>
                                             Q5. How can I adopt my Cozy Capy?
@@ -102,9 +104,10 @@ const Faqs = () => {
                                         </Accordion.Body>
                                     </Accordion.Item>
                                     <Accordion.Item
-                                                    data-aos="zoom-in"
-                                                    data-aos-delay="600"
-                                                    data-aos-anchor-placement="center-bottom"
+                                     eventKey="5"
+                                        data-aos="zoom-in"
+                                        data-aos-delay="600"
+                                        data-aos-anchor-placement="center-bottom"
                                     >
                                         <Accordion.Header>
                                             Q6. What utility does owning a Cozy Capy provide?
